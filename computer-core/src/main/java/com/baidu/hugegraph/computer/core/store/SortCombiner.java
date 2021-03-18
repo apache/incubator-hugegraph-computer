df826c082984ae4d8aa8024236baf0301581f5db
houzhizhen <houzhizhen_101@163.com>
2021-03-18 09:33:32 +0800

    add sort interface (#19)
    * first version of sorter
    
    * add sort interface
    
    * remove first version of sorter

---
+++ computer-core/src/main/java/com/baidu/hugegraph/computer/core/store/SortCombiner.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2017 HugeGraph Authors
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.baidu.hugegraph.computer.core.store;
+
+import java.util.List;
+
+import com.baidu.hugegraph.computer.core.io.RandomAccessOutput;
+
+public interface SortCombiner {
+
+    /**
+     * Combine the list of inputs, and write the combined result length and
+     * results to output.
+     */
+    void combine(List<Pointer> inputs, RandomAccessOutput output);
+}
