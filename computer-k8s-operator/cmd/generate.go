03d68db4284a360b2c87343a56df82069944946f
coderzc <zc1217zc@126.com>
2021-06-06 21:18:26 +0800

    improve code

--- computer-k8s-operator/cmd/generate.go
+++ computer-k8s-operator/cmd/generate.go
@@ -1,18 +1,22 @@
-/**
- * Copyright (C) 2015 Red Hat, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+Copyright 2017 HugeGraph Authors
+
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements. See the NOTICE file distributed with this
+work for additional information regarding copyright ownership. The ASF
+licenses this file to You under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+*/
+
 package main
 
 import (
@@ -43,7 +47,8 @@ func main() {
 	// types that are manually defined in the model
 	providedTypes := []schemagen.ProvidedType{}
 
-	// go packages that are provided and where no generation is required and their corresponding java package
+	// go packages that are provided and where no
+	// generation is required and their corresponding java package
 	providedPackages := map[string]string{
 		// external
 		"k8s.io/api/core/v1":                   "io.fabric8.kubernetes.api.model",
@@ -53,7 +58,8 @@ func main() {
 	}
 
 	// mapping of go packages of this module to the resulting java package
-	// optional ApiGroup and ApiVersion for the go package (which is added to the generated java class)
+	// optional ApiGroup and ApiVersion for the go package
+	// (which is added to the generated java class)
 	packageMapping := map[string]schemagen.PackageInformation{
 		// v1
 		"computer.hugegraph.io/operator/api/v1": {
@@ -64,13 +70,7 @@ func main() {
 	}
 
 	// converts all packages starting with <key> to
-	// a java package using an automated scheme:
-	//  - replace <key> with <value> aka "package prefix"
-	//  - replace '/' with '.' for a valid java package name
-	// e.g. github.com/apache/camel-k/pkg/apis/camel/v1/knative/CamelEnvironment is mapped to "io.fabric8.camelk.internal.pkg.apis.camel.v1.knative.CamelEnvironment"
-	mappingSchema := map[string]string{
-		//"github.com/apache/camel-k/pkg/apis/camel/v1/knative": "io.fabric8.camelk.v1beta1.internal",
-	}
+	mappingSchema := map[string]string{}
 
 	// overwriting some times
 	manualTypeMap := map[reflect.Type]string{
