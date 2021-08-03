9974968257de3db8735fc85b46980c68063e8583
coderzc <zc1217zc@126.com>
2021-08-03 16:17:58 +0800

    add k8s operator test

--- computer-test/src/main/java/com/baidu/hugegraph/computer/k8s/MiniKubeTest.java
+++ computer-test/src/main/java/com/baidu/hugegraph/computer/k8s/MiniKubeTest.java
@@ -25,6 +25,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -45,6 +46,7 @@ import com.baidu.hugegraph.computer.k8s.config.KubeDriverOptions;
 import com.baidu.hugegraph.computer.k8s.config.KubeSpecOptions;
 import com.baidu.hugegraph.computer.k8s.crd.model.HugeGraphComputerJob;
 import com.baidu.hugegraph.computer.k8s.driver.KubernetesDriver;
+import com.baidu.hugegraph.computer.k8s.operator.common.AbstractController;
 import com.baidu.hugegraph.computer.k8s.operator.config.OperatorOptions;
 import com.baidu.hugegraph.computer.k8s.util.KubeUtil;
 import com.baidu.hugegraph.computer.suite.unit.UnitTestBase;
@@ -52,6 +54,7 @@ import com.baidu.hugegraph.testutil.Assert;
 import com.baidu.hugegraph.testutil.Whitebox;
 import com.google.common.collect.Lists;
 
+import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.Quantity;
 
 public class MiniKubeTest extends AbstractK8sTest {
@@ -308,6 +311,41 @@ public class MiniKubeTest extends AbstractK8sTest {
 
         String diagnostics = this.driver.diagnostics(jobId, params);
         Assert.assertContains("ImagePullBackOff", diagnostics);
+
+        future.getNow(null);
+    }
+
+    @Test
+    public void testGetResourceListWithLabels() {
+        Map<String, String> params = new HashMap<>();
+        params.put(KubeSpecOptions.WORKER_INSTANCES.name(), "1");
+        params.put(ComputerOptions.TRANSPORT_SERVER_PORT.name(), "0");
+        params.put(ComputerOptions.RPC_SERVER_PORT.name(), "0");
+        String jobId = this.driver.submitJob(ALGORITHM_NAME, params);
+
+        JobObserver jobObserver = Mockito.mock(JobObserver.class);
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            this.driver.waitJob(jobId, params, jobObserver);
+        });
+
+        DefaultJobState jobState = new DefaultJobState();
+        jobState.jobStatus(JobStatus.INITIALIZING);
+        Mockito.verify(jobObserver, Mockito.timeout(15000L).atLeast(1))
+               .onJobStateChanged(Mockito.eq(jobState));
+
+        List<AbstractController<?>> controllers = Whitebox.getInternalState(
+                                                  this.entrypoint,
+                                                  "controllers");
+        AbstractController<?> abstractController = controllers.get(0);
+        List<Pod> pods = Whitebox.invoke(AbstractController.class,
+                                         new Class[]{String.class, Class.class,
+                                                     Map.class},
+                                         "getResourceListWithLabels",
+                                         abstractController,
+                                         this.namespace, Pod.class,
+                                         new HashMap<String, String>());
+        Assert.assertNotEquals(0, pods.size());
 
         future.getNow(null);
     }
