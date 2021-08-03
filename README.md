966beb3eef65243284ddc6b9d0c4deb83fe465fa
coderzc <zc1217zc@126.com>
2021-08-03 17:02:40 +0800

    improve k8s operator test (#76)
    * improve k8s operator test
    
    * * improve k8s operator test
    * modify ci badge
    
    * add k8s operator test

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # hugegraph-computer
 
 [![License](https://img.shields.io/badge/license-Apache%202-0E78BA.svg)](https://www.apache.org/licenses/LICENSE-2.0.html)
-[![Build Status](https://travis-ci.org/hugegraph-ee/hugegraph-computer.svg?branch=master)](https://travis-ci.org/hugegraph-ee/hugegraph-computer)
-[![codecov](https://codecov.io/gh/hugegraph-ee/hugegraph-computer/branch/master/graph/badge.svg)](https://codecov.io/gh/hugegraph-ee/hugegraph-computer)
+[![Build Status](https://github.com/hugegraph/hugegraph-computer/actions/workflows/ci.yml/badge.svg)](https://github.com/hugegraph/hugegraph-computer/actions/workflows/ci.yml)
+[![codecov](https://codecov.io/gh/hugegraph/hugegraph-computer/branch/master/graph/badge.svg)](https://codecov.io/gh/hugegraph/hugegraph-computer)
 
 hugegraph-computer is a distributed graph processing system for hugegraph. It is an implementaion of [Pregel](https://kowshik.github.io/JPregel/pregel_paper.pdf). It runs on Kubernetes or YARN framework.
 
--- computer-test/src/main/java/com/baidu/hugegraph/computer/k8s/MiniKubeTest.java
+++ computer-test/src/main/java/com/baidu/hugegraph/computer/k8s/MiniKubeTest.java
@@ -25,6 +25,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
@@ -41,9 +42,11 @@ import com.baidu.hugegraph.computer.driver.DefaultJobState;
 import com.baidu.hugegraph.computer.driver.JobObserver;
 import com.baidu.hugegraph.computer.driver.JobStatus;
 import com.baidu.hugegraph.computer.driver.config.ComputerOptions;
+import com.baidu.hugegraph.computer.k8s.config.KubeDriverOptions;
 import com.baidu.hugegraph.computer.k8s.config.KubeSpecOptions;
 import com.baidu.hugegraph.computer.k8s.crd.model.HugeGraphComputerJob;
 import com.baidu.hugegraph.computer.k8s.driver.KubernetesDriver;
+import com.baidu.hugegraph.computer.k8s.operator.common.AbstractController;
 import com.baidu.hugegraph.computer.k8s.operator.config.OperatorOptions;
 import com.baidu.hugegraph.computer.k8s.util.KubeUtil;
 import com.baidu.hugegraph.computer.suite.unit.UnitTestBase;
@@ -51,6 +54,7 @@ import com.baidu.hugegraph.testutil.Assert;
 import com.baidu.hugegraph.testutil.Whitebox;
 import com.google.common.collect.Lists;
 
+import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.Quantity;
 
 public class MiniKubeTest extends AbstractK8sTest {
@@ -283,6 +287,65 @@ public class MiniKubeTest extends AbstractK8sTest {
         this.driver.cancelJob(jobId, params);
 
         UnitTestBase.sleep(1000L);
+
+        future.getNow(null);
+    }
+
+    @Test
+    public void testPullImageError() {
+        Map<String, String> params = new HashMap<>();
+        this.updateOptions(KubeDriverOptions.IMAGE_REPOSITORY_URL.name(),
+                           "xxx");
+        String jobId = this.driver.submitJob(ALGORITHM_NAME, params);
+
+        JobObserver jobObserver = Mockito.mock(JobObserver.class);
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            this.driver.waitJob(jobId, params, jobObserver);
+        });
+
+        DefaultJobState jobState = new DefaultJobState();
+        jobState.jobStatus(JobStatus.FAILED);
+        Mockito.verify(jobObserver, Mockito.timeout(30000L).atLeast(1))
+               .onJobStateChanged(Mockito.eq(jobState));
+
+        String diagnostics = this.driver.diagnostics(jobId, params);
+        Assert.assertContains("ImagePullBackOff", diagnostics);
+
+        future.getNow(null);
+    }
+
+    @Test
+    public void testGetResourceListWithLabels() {
+        Map<String, String> params = new HashMap<>();
+        params.put(KubeSpecOptions.WORKER_INSTANCES.name(), "1");
+        params.put(ComputerOptions.TRANSPORT_SERVER_PORT.name(), "0");
+        params.put(ComputerOptions.RPC_SERVER_PORT.name(), "0");
+        String jobId = this.driver.submitJob(ALGORITHM_NAME, params);
+
+        JobObserver jobObserver = Mockito.mock(JobObserver.class);
+
+        CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+            this.driver.waitJob(jobId, params, jobObserver);
+        });
+
+        DefaultJobState jobState = new DefaultJobState();
+        jobState.jobStatus(JobStatus.INITIALIZING);
+        Mockito.verify(jobObserver, Mockito.timeout(15000L).atLeast(1))
+               .onJobStateChanged(Mockito.eq(jobState));
+
+        List<AbstractController<?>> controllers = Whitebox.getInternalState(
+                                                  this.entrypoint,
+                                                  "controllers");
+        AbstractController<?> abstractController = controllers.get(0);
+        List<Pod> pods = Whitebox.invoke(AbstractController.class,
+                                         new Class[]{String.class, Class.class,
+                                                     Map.class},
+                                         "getResourceListWithLabels",
+                                         abstractController,
+                                         this.namespace, Pod.class,
+                                         new HashMap<String, String>());
+        Assert.assertNotEquals(0, pods.size());
 
         future.getNow(null);
     }
