96c839159fbe918052ebc5e497e05a63ae72ae67
coderzc <zc1217zc@126.com>
2021-08-11 22:31:53 +0800

    remove etcd

--- computer-dist/src/assembly/travis/start-etcd.sh
+++ computer-dist/src/assembly/travis/start-etcd.sh
@@ -6,7 +6,7 @@ TRAVIS_DIR=`dirname $0`
 echo "Starting etcd..."
 wget -O ${TRAVIS_DIR}/etcd.tar.gz https://github.com/etcd-io/etcd/releases/download/v3.5.0/etcd-v3.5.0-linux-amd64.tar.gz
 mkdir ${TRAVIS_DIR}/etcd
-tar -zxvf ${TRAVIS_DIR}/etcd.tar.gz -C ${TRAVIS_DIR}/etcd
+tar -zxvf ${TRAVIS_DIR}/etcd.tar.gz -C ${TRAVIS_DIR}/etcd --strip-components 1
 chmod a+x ${TRAVIS_DIR}/etcd
 ${TRAVIS_DIR}/etcd/etcd --name etcd-test \
     --initial-advertise-peer-urls http://localhost:2580 \
