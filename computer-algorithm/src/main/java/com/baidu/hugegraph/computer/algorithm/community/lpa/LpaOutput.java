60951a221f2034e9054183b21d5d11e2a2dbfdcf
ShouJing <1075185785@qq.com>
2021-10-22 16:39:50 +0800

    add lpa output (#20)

---
+++ computer-algorithm/src/main/java/com/baidu/hugegraph/computer/algorithm/community/lpa/LpaOutput.java
@@ -0,0 +1,32 @@
+package com.baidu.hugegraph.computer.algorithm.community.lpa;
+
+import com.baidu.hugegraph.computer.core.graph.vertex.Vertex;
+import com.baidu.hugegraph.computer.core.output.hg.HugeOutput;
+import com.baidu.hugegraph.structure.constant.WriteType;
+
+public class LpaOutput extends HugeOutput {
+
+    @Override
+    public String name() {
+        return "lpa";
+    }
+
+    @Override
+    public void prepareSchema() {
+        this.client().schema().propertyKey(this.name())
+                     .asText()
+                     .writeType(WriteType.OLAP_COMMON)
+                     .ifNotExist()
+                     .create();
+    }
+
+    @Override
+    public com.baidu.hugegraph.structure.graph.Vertex constructHugeVertex(
+            Vertex vertex) {
+        com.baidu.hugegraph.structure.graph.Vertex hugeVertex =
+                new com.baidu.hugegraph.structure.graph.Vertex(null);
+        hugeVertex.id(vertex.id().asObject());
+        hugeVertex.property(this.name(), vertex.value().toString());
+        return hugeVertex;
+    }
+}
