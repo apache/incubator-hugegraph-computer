82de16a9bcd48eec6867bb802ce391156bcd538e
ShouJing <1075185785@qq.com>
2021-12-23 18:15:26 +0800

    delete unused code

--- computer-test/src/main/java/com/baidu/hugegraph/computer/core/compute/ComputeManagerTest.java
+++ computer-test/src/main/java/com/baidu/hugegraph/computer/core/compute/ComputeManagerTest.java
@@ -19,21 +19,15 @@
 
 package com.baidu.hugegraph.computer.core.compute;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.function.Consumer;
 
-import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.baidu.hugegraph.computer.core.combiner.EdgeValueCombiner;
-import com.baidu.hugegraph.computer.core.combiner.AbstractPointerCombiner;
 import com.baidu.hugegraph.computer.core.common.Constants;
 import com.baidu.hugegraph.computer.core.config.ComputerOptions;
 import com.baidu.hugegraph.computer.core.config.Config;
@@ -51,8 +45,6 @@ import com.baidu.hugegraph.computer.core.graph.vertex.Vertex;
 import com.baidu.hugegraph.computer.core.io.BytesOutput;
 import com.baidu.hugegraph.computer.core.io.GraphComputeOutput;
 import com.baidu.hugegraph.computer.core.io.IOFactory;
-import com.baidu.hugegraph.computer.core.io.RandomAccessInput;
-import com.baidu.hugegraph.computer.core.io.StreamGraphInput;
 import com.baidu.hugegraph.computer.core.io.StreamGraphOutput;
 import com.baidu.hugegraph.computer.core.manager.Managers;
 import com.baidu.hugegraph.computer.core.network.ConnectionId;
@@ -61,19 +53,11 @@ import com.baidu.hugegraph.computer.core.network.message.MessageType;
 import com.baidu.hugegraph.computer.core.receiver.MessageRecvManager;
 import com.baidu.hugegraph.computer.core.receiver.ReceiverUtil;
 import com.baidu.hugegraph.computer.core.sender.MessageSendManager;
-import com.baidu.hugegraph.computer.core.sort.Sorter;
-import com.baidu.hugegraph.computer.core.sort.flusher.CombineSubKvOuterSortFlusher;
-import com.baidu.hugegraph.computer.core.sort.flusher.OuterSortFlusher;
 import com.baidu.hugegraph.computer.core.sort.sorting.SendSortManager;
 import com.baidu.hugegraph.computer.core.sort.sorting.SortManager;
 import com.baidu.hugegraph.computer.core.store.FileManager;
-import com.baidu.hugegraph.computer.core.store.hgkvfile.buffer.EntryIterator;
 import com.baidu.hugegraph.computer.core.store.hgkvfile.entry.EntryOutput;
 import com.baidu.hugegraph.computer.core.store.hgkvfile.entry.EntryOutputImpl;
-import com.baidu.hugegraph.computer.core.store.hgkvfile.entry.KvEntry;
-import com.baidu.hugegraph.computer.core.store.hgkvfile.entry.Pointer;
-import com.baidu.hugegraph.computer.core.store.hgkvfile.file.reader.HgkvDirReader;
-import com.baidu.hugegraph.computer.core.store.hgkvfile.file.reader.HgkvDirReaderImpl;
 import com.baidu.hugegraph.computer.core.worker.Computation;
 import com.baidu.hugegraph.computer.suite.unit.UnitTestBase;
 import com.baidu.hugegraph.testutil.Whitebox;
@@ -249,68 +233,6 @@ public class ComputeManagerTest extends UnitTestBase {
                 ReceiverUtil.consumeBuffer(ReceiverUtil.writeMessage(id,
                                                                      message),
                                            consumer);
-            }
-        }
-    }
-
-    @Test
-    public void test() throws Exception {
-        List<RandomAccessInput> buffers = new ArrayList<>();
-        for (long i = 0L; i < 3; i++) {
-            Vertex vertex = graphFactory().createVertex();
-            vertex.id(BytesId.of(1));
-            Edges edges = graphFactory().createEdges(3);
-
-            for (long j = 0; j < 3; j++) {
-                Edge edge = graphFactory().createEdge();
-                edge.targetId(BytesId.of(j));
-                edge.label("hello");
-                Properties properties = graphFactory().createProperties();
-                properties.put("p1", new LongValue(0));
-                edge.properties(properties);
-                edges.add(edge);
-            }
-            vertex.edges(edges);
-            byte[] bytes = writeEdges(vertex, EdgeFrequency.SINGLE);
-            buffers.add(IOFactory.createBytesInput(bytes));
-        }
-
-        SortManager sorter = this.managers.get("send_sort");
-
-        AbstractPointerCombiner combiner = new EdgeValueCombiner(context());
-        OuterSortFlusher flusher = new CombineSubKvOuterSortFlusher(combiner,
-                                                                    5);
-        String path = "~/test/a";
-        FileUtils.deleteDirectory(new File(path));
-        Sorter sorter1 = Whitebox.getInternalState(sorter, "sorter");
-        flusher.sources(buffers.size());
-        sorter1.mergeBuffers(buffers, flusher, path, true);
-
-        HgkvDirReader reader = new HgkvDirReaderImpl(path, true);
-        EntryIterator iterator = reader.iterator();
-        while (iterator.hasNext()) {
-            KvEntry next = iterator.next();
-            Pointer key = next.key();
-            Pointer value = next.value();
-
-            BytesId bytesId = new BytesId();
-            bytesId.read(key.input());
-
-            RandomAccessInput input = value.input();
-            int count = input.readFixedInt();
-            Edges edges = graphFactory().createEdges(count);
-            for (int i = 0; i < count; i++) {
-                Edge edge = graphFactory().createEdge();
-                // Only use targetId as subKey, use props as subValue
-                input.readFixedInt();
-                edge.targetId(StreamGraphInput.readId(input));
-                // Read subValue
-                input.readFixedInt();
-                Properties props = graphFactory().createProperties();
-                props.read(input);
-                edge.properties(props);
-                edge.label(StreamGraphInput.readLabel(input));
-                edges.add(edge);
             }
         }
     }
