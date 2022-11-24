394b1b9f2ea7c540fec7a69f8b67ca22040e420c
coderzc <zhaocong@apache.org>
2022-11-25 01:03:16 +0800

    improve build image

--- computer-dist/src/assembly/travis/build-images.sh
+++ computer-dist/src/assembly/travis/build-images.sh
@@ -35,10 +35,4 @@ PROJECT_VERSION=$(mvn -f "${PROJECT_POM_PATH}" -q -N \
 
 docker build -t $1 $CONTEXT_PATH -f $PROJECT_PATH/computer-dist/Dockerfile
 
-echo "FROM $1
-LABEL maintainer='HugeGraph Docker Maintainers <dev@hugegraph.apache.org>'
-COPY target/computer-algorithm-*.jar $JAR_FILE_PATH
-ENV JAR_FILE_PATH=$JAR_FILE_PATH" | \
-docker build -t $2 -f - $PROJECT_PATH/computer-algorithm
-
-docker build -t $3 -f $PROJECT_PATH/computer-k8s-operator/Dockerfile $PROJECT_PATH/computer-k8s-operator
+docker build -t $2 -f $PROJECT_PATH/computer-k8s-operator/Dockerfile $PROJECT_PATH/computer-k8s-operator
