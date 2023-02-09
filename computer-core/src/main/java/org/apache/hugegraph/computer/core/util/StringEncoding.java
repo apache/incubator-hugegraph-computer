b1f734636c8d23debb486bb1bac6393729732eec
Simon Cheung <ming@apache.org>
2023-02-09 19:35:22 +0800

    chore: fix StringEncoding license & add multi license check action (#227)
    * add license check action & use proper "UTF-8" charset
    
    * exclude generate go files
    
    * include release-docs in binary release
    
    * Update NOTICE
    
    ---------
    
    Co-authored-by: imbajin <jin@apache.org>

--- computer-core/src/main/java/org/apache/hugegraph/computer/core/util/StringEncoding.java
+++ computer-core/src/main/java/org/apache/hugegraph/computer/core/util/StringEncoding.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
 // Copyright 2017 JanusGraph Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,7 +14,7 @@
 
 package org.apache.hugegraph.computer.core.util;
 
-import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
@@ -44,7 +28,6 @@ import com.google.common.base.CharMatcher;
 
 /**
  * @author Matthias Broecheler (me@matthiasb.com)
- * @author HugeGraph Authors
  */
 public final class StringEncoding {
 
@@ -63,7 +46,7 @@ public final class StringEncoding {
     private static final Base64.Encoder BASE64_ENCODER = Base64.getEncoder();
     private static final Base64.Decoder BASE64_DECODER = Base64.getDecoder();
 
-    // Similar to {@link StringSerializer}
+    /** Similar to {@link StringSerializer} */
     public static int writeAsciiString(byte[] array, int offset, String value) {
         E.checkArgument(CharMatcher.ascii().matchesAllOf(value),
                         "'%s' must be ASCII string", value);
@@ -79,7 +62,8 @@ public final class StringEncoding {
             assert c <= 127;
             byte b = (byte) c;
             if (++i == len) {
-                b |= 0x80; // End marker
+                // End marker
+                b |= 0x80;
             }
             array[offset++] = b;
         } while (i < len);
@@ -89,7 +73,7 @@ public final class StringEncoding {
 
     public static String readAsciiString(byte[] array, int offset) {
         StringBuilder sb = new StringBuilder();
-        int c = 0;
+        int c;
         do {
             c = 0xFF & array[offset++];
             if (c != 0x80) {
@@ -106,27 +90,15 @@ public final class StringEncoding {
     }
 
     public static byte[] encode(String value) {
-        try {
-            return value.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new ComputerException("Failed to encode string", e);
-        }
+        return value.getBytes(StandardCharsets.UTF_8);
     }
 
     public static String decode(byte[] bytes) {
-        try {
-            return new String(bytes, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new ComputerException("Failed to decode string", e);
-        }
+        return new String(bytes, StandardCharsets.UTF_8);
     }
 
     public static String decode(byte[] bytes, int offset, int length) {
-        try {
-            return new String(bytes, offset, length, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new ComputerException("Failed to decode string", e);
-        }
+        return new String(bytes, offset, length, StandardCharsets.UTF_8);
     }
 
     public static String encodeBase64(byte[] bytes) {
@@ -157,8 +129,7 @@ public final class StringEncoding {
                 return UUID.fromString(value);
             }
             // UUID represented by hex string
-            E.checkArgument(value.length() == 32,
-                            "Invalid UUID string: %s", value);
+            E.checkArgument(value.length() == 32, "Invalid UUID string: %s", value);
             String high = value.substring(0, 16);
             String low = value.substring(16);
             return new UUID(Long.parseUnsignedLong(high, 16),
