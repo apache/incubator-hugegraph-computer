2e1221f38015b8716fabd9f521c278c2b26e3ac1
imbajin <jin@apache.org>
2023-12-05 20:11:19 +0800

    Update load-data-into-hugegraph.sh

--- computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
+++ computer-dist/src/assembly/travis/load-data-into-hugegraph.sh
@@ -39,10 +39,9 @@ unzip -d ${DATASET_DIR} ml-latest-small.zip
 cd ${DATASET_DIR}/.. && pwd && ls -lh *
 docker run -id --name=loader hugegraph/loader:latest
 docker cp dataset loader:/dataset
-docker exec -i loader ls -lh /dataset
 
-docker exec -i loader bin/hugegraph-loader.sh -g hugegraph -f /dataset/struct.json \
-    -s /dataset/schema.groovy -h graph -p 8080
+docker exec -i loader ls -lh /dataset && bin/hugegraph-loader.sh -g hugegraph -p 8080 \
+    -f /dataset/struct.json -s /dataset/schema.groovy -h 127.0.0.1
 
 #hugegraph-toolchain/hugegraph-loader/apache-hugegraph-loader-*/bin/hugegraph-loader.sh \
 #    -g hugegraph -f ${DATASET_DIR}/struct.json -s ${DATASET_DIR}/schema.groovy || exit 1
