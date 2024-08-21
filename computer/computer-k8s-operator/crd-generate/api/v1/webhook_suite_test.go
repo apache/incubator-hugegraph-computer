4dd8f093c43f63cbce48b1ef21a9e401d8fc1165
coderzc <zhaocong@apache.org>
2024-08-21 20:34:53 +0800

    fix

--- computer/computer-k8s-operator/crd-generate/api/v1/webhook_suite_test.go
+++ computer/computer-k8s-operator/crd-generate/api/v1/webhook_suite_test.go
@@ -26,6 +26,9 @@ import (
 	"testing"
 	"time"
 
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
 	admissionv1 "k8s.io/api/admission/v1"
 	//+kubebuilder:scaffold:imports
 	"k8s.io/apimachinery/pkg/runtime"
