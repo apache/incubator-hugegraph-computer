8327b2e9cebc41bfd8f35ee854f229d388f30669
wenyuxuan <wenyuxuan@baidu.com>
2025-04-27 17:15:39 +0800

    Change dependent `github.com/antonmedv/expr` to `github.com/expr-lang/expr`

--- vermeer/apps/compute/filter.go
+++ vermeer/apps/compute/filter.go
@@ -23,8 +23,8 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/antonmedv/expr"
-	"github.com/antonmedv/expr/vm"
+	"github.com/expr-lang/expr"
+	"github.com/expr-lang/expr/vm"
 )
 
 type VertexFilter struct {
